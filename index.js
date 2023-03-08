97aa4d7594f8976fe2c29d9bce7759ade2b96282
nicholas <tohnic1999@gmail.com>
2023-03-09 00:37:03 +0800

    Return hash only after 1 second

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express()
 const crypto = require('crypto')
 const port = 3000
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 app.get('/', async (req, res) => {
+    // Start the timer
+    const promise = sleep(1000);
+
+    // Create hash with current time
     const hash = crypto.createHash('sha256')
     hash.update(`${new Date().getTime()}`)
+
+    // Wait for timer to finish
+    await promise;
+
+    // Send hash as hex
     res.send(hash.digest('hex'))
 })
 
